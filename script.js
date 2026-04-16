7eaf4e8ddb5e605e885348d31d23170a7f828a20
thukusam053-stack <thukusam053@gmail.com>
2026-04-16 12:27:55 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,17 @@
 let display = document.getElementById("display");
 
-// Add number/operator
 function appendValue(value) {
   display.value += value;
 }
 
-// Clear everything
 function clearDisplay() {
   display.value = "";
 }
 
-// Delete last character
 function deleteLast() {
   display.value = display.value.slice(0, -1);
 }
 
-// Calculate result
 function calculateResult() {
   try {
     display.value = eval(display.value);
